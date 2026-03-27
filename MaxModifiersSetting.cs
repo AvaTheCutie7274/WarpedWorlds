ff52a03fbc1f68e4c9002452997cbf3af19d9061
AvaTheCutie <74994063+AvaTheCutie7274@users.noreply.github.com>
2026-03-27 18:41:08 -0400

    Add MaxModifiersSetting class for HasteEffects

--- MaxModifiersSetting.cs
+++ MaxModifiersSetting.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
+using UnityEngine.Localization;
+using Zorro.Settings;
 
+namespace HasteEffects;
+
+[HasteSetting]
+public class MaxModifiersSetting : IntSetting, IExposedSetting
+{
+    public string GetCategory() => "HasteEffects";
+
+    public LocalizedString GetDisplayName() =>
+        new UnlocalizedString("Max Modifiers");
+
+    protected override int GetDefaultValue() => 4;
+
+    public override void ApplyValue()
+    {
+        Debug.Log($"HasteEffects: Max Modifiers = {Value}");
+    }
+}
